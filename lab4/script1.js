1de3f4b2351c514598d425cd622e0deda8ae2b21
AntonioLanderos <juan.alanderosv@outlook.com>
2024-04-07 12:09:43 -0600

    Se agregaron estilos css al html

--- lab4/script1.js
+++ lab4/script1.js
@@ -7,7 +7,7 @@ document.write("<tr><th>Numero</th><th>Cuadrado</th><th>Cubo</th></tr>");
 for (let i = 1; i <= num; i++)
 {
     var cuadrado = i * i; 
-    var cubo = i *i * i; 
+    var cubo = i * i * i; 
     document.write("<tr><td>" + i + "</td><td>" + cuadrado + "</td><td>" + cubo + "</td></tr>");
 }
 
